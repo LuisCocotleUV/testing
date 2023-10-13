7b014f4d6adb932e0b312e59f044f2b19e59d3ba
LuisCocotleUV <luisdavidcocoyuv@gmail.com>
2023-10-13 11:20:33 -0600

    stddev service added - localhost version

--- src/app/stddev/stddev.component.spec.ts
+++ src/app/stddev/stddev.component.spec.ts
@@ -1,31 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StddevComponent } from './stddev.component';
+import { StddevService } from '../services/stddev.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('StddevComponent', () => {
   let component: StddevComponent;
   let fixture: ComponentFixture<StddevComponent>;
+  let stddevService: StddevService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [StddevComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [StddevComponent],
+      providers: [StddevService],
+      imports: [HttpClientModule]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(StddevComponent);
     component = fixture.componentInstance;
+    stddevService = TestBed.inject(StddevService);
+  });
+
+  it('Should return stddev=62.26 with the 1a_dev_hours.json', async () => {
     fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.devHoursStddev).toBeCloseTo(62.26, 2);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('Should return stddev=572.03 with the 1a_proxy_size.json', async () => {
+    fixture.detectChanges();
+    await fixture.whenStable();
 
-  it('Should return stddev = 572.03 with the data: 160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503', () => {
-    component.data = [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503];
-    expect(component.calculateStdDev()).toBeCloseTo(572.03, 2);
+    expect(component.proxySizeStddev).toBeCloseTo(572.03, 2);
   });
-
-  it('Should return stddev = 62.26 with the data: 15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2', () => {
-    component.data = [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2];
-    expect(component.calculateStdDev()).toBeCloseTo(62.26,2);
-  });
-})
+});
--- src/app/stddev/stddev.component.ts
+++ src/app/stddev/stddev.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StddevService } from '../services/stddev.service';
 
 @Component({
   selector: 'app-stddev',
-  templateUrl: './stddev.component.html',
-  styleUrls: ['./stddev.component.css']
+  template: ''
 })
-export class StddevComponent {
-  data: number[] = [];
+export class StddevComponent implements OnInit {
+  devHoursStddev: number = 0;
+  proxySizeStddev: number = 0;
 
-  calculateStdDev(): number {
-    let sum = this.data.reduce((a, b) => a + b, 0);
-    let mean = sum / this.data.length;
-  
-    let squareDiffs = this.data.map((value) => Math.pow(value - mean, 2));
-    let variance = squareDiffs.reduce((a, b) => a + b, 0) / (this.data.length - 1);
-  
-    return Math.sqrt(variance);
-  }  
+  constructor(private stddevService: StddevService) {}
+
+  ngOnInit(): void {
+    this.stddevService.getDevHours().subscribe(data => {
+      this.devHoursStddev = this.calcularDesviacionEstandar(data);
+    });
+
+    this.stddevService.getProxySize().subscribe(data => {
+      this.proxySizeStddev = this.calcularDesviacionEstandar(data);
+    });
+  }
+
+  private calcularDesviacionEstandar(data: number[]): number {
+    const n = data.length;
+    const mean = data.reduce((acc, val) => acc + val, 0) / n;
+    const sumSquaredDiff = data.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0);
+    return Math.sqrt(sumSquaredDiff / (n - 1));
+  }
 }
