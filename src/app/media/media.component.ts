484540cfdc01ff9a0428fe4bb88896e506e1c13e
LuisCocotleUV <luisdavidcocoyuv@gmail.com>
2023-10-13 01:21:25 -0600

    fixed media

--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -1,10 +1,13 @@
-export function media (array:number[]) {
-  var total=0, result, numDatos, item;
-  numDatos = array.length;
-  for (item = 0; item < numDatos; item++){
-      total += array[item];
-  }
-  result = total/numDatos;
+import { Component } from '@angular/core';
 
-  return result;
+@Component({
+  selector: 'app-media',
+  templateUrl: './media.component.html',
+  styleUrls: ['./media.component.css']
+})
+export class MediaComponent {
+  calculateMean(data: number[]): number {
+    let sum = data.reduce((a, b) => a + b, 0);
+    return sum / data.length;
+  }
 }
